48ab0b5da5d4d9d0c35116a65135c66e583efe56
ranggadsaputra <ranggadsaputra11@gmail.com>
2025-03-04 16:45:31 +0700

    first commit

---
+++ app/Http/Controllers/LevelController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class LevelController extends Controller
+{
+    public function index() {
+        // DB::insert('insert into m_level(level_kode, level_nama, created_at) values(?, ?, ?)', ['cus', 'Pelanggan', now()]);
+        // return 'Insert data baru berhasil';
+
+        // $row =DB::update('update m-level set level_nama = ? where level_kode=?', ['Custmoer', 'cus']);
+        // return 'update data berhasil. Jumlah data yang diupdate: ' .$row. ' baris';
+        
+        // $row =DB::delete('delete fro m_level where level_kode = ?', ['cus']);
+        // return 'delete data berhasil. Jumlah data yang dihapus: ' .$row. ' baris';
+
+        $data = DB::select('select * from m_level');
+        return view('level', ['data'=> $data]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\LevelController;    
+use App\Http\Controllers\KategoriController;    
+use App\Http\Controllers\UserController;    
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::get('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+Route::get('/user/ubah_simpan{id}', [UserController::class, 'ubah']);
