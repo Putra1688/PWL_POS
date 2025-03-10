799b6132fbc34b3935525e6d536478d8685d8ca9
ranggadsaputra <ranggadsaputra11@gmail.com>
2025-03-10 15:53:45 +0700

    jQuery Datatable di AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,14 @@ Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 Route::get('/user/ubah_simpan{id}', [UserController::class, 'ubah']);   
 
 Route::get('/', [WelcomeController::class,'index']);
+
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class,'index']);           // menampilkan halaman awal user
+    Route::post('/list', [UserController::class,'list']);       // menampilkan data user dalam bntuk json untuk dtabse
+    Route::get('/create', [UserController::class,'create']);    // halaman form tambah user
+    Route::post('/', [UserController::class,'store']);          // menyimpan data user baru
+    Route::get('/{id}', [UserController::class,'show']);        // detail user
+    Route::get('/{id}/edit', [UserController::class,'edit']);   // halaman form edit user
+    Route::put('/{id}', [UserController::class,'update']);      // simpan perubahan data user
+    Route::delete('/{id}', [UserController::class,'destroy']);  // menghapus data user
+});
